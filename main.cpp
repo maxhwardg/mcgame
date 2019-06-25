6ea065ab8ee1d8f774489c958ec0dbd6bc4da3ce
cpyro <connie.pyro@gmail.com>
2019-06-25 05:43:20 -0700

    Add spacemax
    +update main to move spacemax

--- main.cpp
+++ main.cpp
@@ -1,32 +1,51 @@
+//#include <SFML/Audio.hpp>
 #include <SFML/Graphics.hpp>
-#include "gamestate/include/gamestate.h"
+#include <SFML/Window.hpp>
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML works!", sf::Style::Titlebar | sf::Style::Close);
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
-
-    funct();
-
-    sf::View view;
-    view.reset(sf::FloatRect(100, 100, 400, 200));
-    view.rotate(45);
-    window.setView(view);
-
+    // Create the main window
+    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
+    // Load a sprite to display
+    sf::Texture texture;
+    if (!texture.loadFromFile("../resources/spacemax.png"))
+        return EXIT_FAILURE;
+    sf::Sprite sprite(texture);
+    sprite.setScale(0.2, 0.2);
+    // Create a graphical text to display
+//    sf::Font font;
+//    if (!font.loadFromFile("arial.ttf"))
+//        return EXIT_FAILURE;
+//    sf::Text text("Hello SFML", font, 50);
+    // Load a music to play
+//    sf::Music music;
+//    if (!music.openFromFile("Chiptronical.ogg"))
+//        return EXIT_FAILURE;
+    // Play the music
+//    music.play();
+    // Start the game loop
     while (window.isOpen())
     {
+        // Process events
         sf::Event event;
         while (window.pollEvent(event))
         {
+            // Close window: exit
             if (event.type == sf::Event::Closed)
                 window.close();
-        }
 
+            if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
+                sprite.setPosition(sf::Vector2f(sf::Mouse::getPosition(window)));
+            }
+        }
+        // Clear screen
         window.clear();
-        window.draw(shape);
+        // Draw the sprite
+        window.draw(sprite);
+        // Draw the string
+//        window.draw(text);
+        // Update the window
         window.display();
     }
-
-    return 0;
+    return EXIT_SUCCESS;
 }
