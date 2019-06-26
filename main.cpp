18b36c41696fd1ca962f823a200fc6c8c99bd7a7
cpyro <connie.pyro@gmail.com>
2019-06-26 06:08:11 -0700

    neaten tile set files

--- main.cpp
+++ main.cpp
@@ -14,23 +14,23 @@ int main() {
     sf::RenderWindow window(sf::VideoMode(1600, 800), "SFML window");
     window.setFramerateLimit(30);
 
-    mcgame::TileSetManager tile_set_manager(
-            {
-//                    "resources/pixel_grass_tile10.png",
-//                    "resources/pixel_grass_tile10.png",
-                    "resources/pixel_grass_tile9.png",
-                    "resources/pixel_grass_tile8.png",
-                    "resources/pixel_grass_tile7.png",
-                    "resources/pixel_grass_tile6.png",
-                    "resources/pixel_grass_tile5.png",
-                    "resources/pixel_grass_tile4.png",
-                    "resources/pixel_grass_tile3.png",
-                    "resources/pixel_grass_tile3.png",
-                    "resources/pixel_grass_tile3.png",
-                    "resources/pixel_grass_tile2.png",
-                    "resources/pixel_grass_tile.png",
-                    "resources/pixel_grass_tile.png"
-            }, 100, 100, 64);
+    std::vector<std::string> tile_set_files = {
+//           "resources/pixel_grass_tile10.png",
+//           "resources/pixel_grass_tile10.png",
+            "resources/pixel_grass_tile9.png",
+            "resources/pixel_grass_tile8.png",
+            "resources/pixel_grass_tile7.png",
+            "resources/pixel_grass_tile6.png",
+            "resources/pixel_grass_tile5.png",
+            "resources/pixel_grass_tile4.png",
+            "resources/pixel_grass_tile3.png",
+            "resources/pixel_grass_tile3.png",
+            "resources/pixel_grass_tile3.png",
+            "resources/pixel_grass_tile2.png",
+            "resources/pixel_grass_tile.png",
+            "resources/pixel_grass_tile.png"
+    };
+    mcgame::TileSetManager tile_set_manager(tile_set_files, 100, 100, 64);
     std::default_random_engine random_engine;
     for (int i = 0; i < tile_set_manager.Rows(); ++i) {
         for (int j = 0; j < tile_set_manager.Cols(); ++j) {
