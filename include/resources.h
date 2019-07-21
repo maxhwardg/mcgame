5d07db59c8496985b387bcf1657998e9d6fbdea3
maxward <maxhwardg@gmail.com>
2019-07-21 03:09:30 -0700

    Added some commends to the resources file.

--- include/resources.h
+++ include/resources.h
@@ -11,15 +11,16 @@
 namespace mcgame {
     const std::string IMAGE_FOLDER_PATH = "resources/images/";
 
-
     /// All the images known to the game. Must have densely packed ids starting at 0.
     enum ImageName {
         GRASS_TILE = 0,
         FLOATING_SPRITE = 1
     };
 
+    /// Must be equal to the number of ImageName entries.
     const int NUM_IMAGES = 2;
 
+    /// Indexes correspond to ImageName enum values.
     const std::array<std::string, NUM_IMAGES> IMAGE_FILENAMES = {"grass_tile.png", "floating_sprite.png"};
 }
 
